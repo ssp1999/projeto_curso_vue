45783f2dd1b7fd7aec37c521296a1e32dbb64ee1
Shakyra <shakyra.portes@gmail.com>
2023-01-15 21:10:34 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<script>
+import { RouterLink, RouterView } from 'vue-router'
+import NavBar from './components/NavBar.vue';
+import Footer from './components/Footer.vue';
+
+export default {
+    components: {
+        NavBar,
+        Footer
+    },
+    data() {
+        return {
+            logo_src: '/img/logo.png',
+            app_name: 'Make Your Burguer'
+        }
+    }
+}
+</script>
+
+<template>
+  <NavBar :logo="logo_src" :alt="app_name"/>
+  <RouterView />
+  <Footer />
+</template>
+
+<style>
+    * {
+        font-family: Arial, Helvetica, sans-serif;
+        padding: 0;
+        margin: 0;
+        box-sizing: border-box;
+    }
+
+    .main-container {
+        margin: 50px;
+        min-height: 250px;
+    }
+
+    h1 {
+        text-align: center;
+        font-size: 42px;
+        margin-bottom: 30px;
+        color: #222;
+    }
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,19 @@
+<script>
+import Banner from '../components/Banner.vue';
+import BurguerForm from '../components/BurguerForm.vue';
+
+export default {
+    components: {
+    Banner,
+    BurguerForm
+}
+}
+</script>
+
+<template>
+    <Banner />
+    <div class="main-container">
+        <h1>Monte o seu burguer:</h1>
+        <BurguerForm />
+    </div>
+</template>
